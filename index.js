edf82d404bbf84f96be3d1aee6dedc481849ca5d
Aman Tiwari <tiwari95aman@gmail.com>
2025-09-20 14:31:56 +0530

    day 2

--- index.js
+++ index.js
@@ -1,12 +1,59 @@
-const http = require("http");
+// const http = require("http");
+// const fs = require("fs");
 
-const stu = require("./bhopal")
+// http.createServer((req,res)=>{
+//      res.write("<h1>welcome to my page</h1>");
+//      fs.readFile("aman.txt",(err,data)=>{
+//         if(err) throw err;
+//         res.write(data);
+//         res.end("<h3>serverend</h3>");
+//      })
+// }).listen(90)
 
-http.createServer((req,res)=>{
-    res.write("<h1>welcome to node </h1>");
-    res.write(stu.myCollege());
-    res.write(stu.myFees());
-    res.end("serevr end")
-}).listen(8000,()=>{
-   console.log("server is runing on port 8000");
+// const fs = require("fs");
+
+// fs.writeFile("aman.txt","welcome to satna",(err)=>{
+//      if(err) throw err;
+//      console.log("file created");
+// })
+
+// const fs = require("fs");
+
+// fs.appendFile("anuj.txt","hello i am lerning node", (err)=>{
+//     if (err) throw err;
+//     console.log("filw created");
+// })
+
+// const fs = require("fs");
+
+// fs.rename("aman.txt","arvind.txt",(err)=>{
+//     if(err) throw err;
+//     console.log("file renamed");
+// });
+
+// const fs = require("fs");
+
+// fs.unlink("arvind.txt",(err)=>{
+//     if(err) throw err;
+//     console.log("file deleted");
+// });
+
+// const fs = require("fs");
+
+// if(fs.existsSync("anuj.txt")){
+//     console.log("hai bhai")
+// }else{
+//     console.log("file nhi hai")
+// }
+
+const fs = require("fs");
+
+// fs.mkdir("bhopal",(err)=>{
+//     if(err) throw err;
+//     console.log("new folder created");
+// });
+
+fs.readdir(".",(err,files)=>{
+    if(err) throw err;
+    console.log(files);
 })
