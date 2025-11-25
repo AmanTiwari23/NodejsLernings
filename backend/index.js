d7ace3ac0ffc8043a01f79a28561d55eabd34cbf
Aman Tiwari <tiwari95aman@gmail.com>
2025-11-25 14:51:18 +0530

    updated code of cluster

--- backend/index.js
+++ backend/index.js
@@ -40,22 +40,28 @@
 //     console.log("server run on 8000! port")
 // });
 
-const http = require("http");
-const cluster = require("cluster");
-const os = require("os");
-if(cluster.isMaster) {
-  console.log("master is processing");
-  console.log(os.cpus().length);
-  cluster.fork();
-  cluster.fork();
-  cluster.fork();
+const cluster = require('cluster');
+const http = require('http');
+const os = require('os');
+if (cluster.isMaster) {
+// Get the number of CPU cores available
+const numCPUs = os.cpus().length;
+console.log(`Master process PID: ${process.pid}`);
+console.log(`Forking ${numCPUs} workers...`);
+// Fork workers
+for (let i = 0; i < numCPUs; i++) {
+cluster.fork();
+}
+// Listen for worker exit events
+cluster.on('exit', (worker, code, signal) => {
+console.log(`Worker ${worker.process.pid} died. Forking a new worker...`);
+cluster.fork();
+});
 } else {
-  http.createServer((req, res) => {
-      setTimeout(() => {
-        res.write("<h1>Request handles successfuly</h1>");
-        res.end();
-      }, 5000);
-    })
-    .listen(3000);
-  console.log("serer is runing on 3000");
+// Worker processes handle incoming HTTP requests
+http.createServer((req, res) => {
+res.writeHead(200);
+res.end(`Handled by worker process PID: ${process.pid}\n`);
+}).listen(3000);
+console.log(`Worker process PID: ${process.pid} is running`);
 }
